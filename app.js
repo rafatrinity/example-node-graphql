3ff8aa300d81018c0eee926705fac274d3223c80
Rafael Da Trindade <rafael.da.trindade@avanade.com>
2020-04-25 19:50:05 -0300

    Hello World!

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var express_graphql = require("express-graphql");
+var {
+    buildSchema
+} = require("graphql");
+
+// Schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Mapeamento
+var root = {
+    message: () => 'Hello World!'
+};
+
+
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(
+    3000, () =>
+    console.log('Express GraphQL Server Now Running On localhost:3000/graphql')
+);
